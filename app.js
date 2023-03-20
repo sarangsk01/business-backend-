9ff2bda7120e5e359e2676d56f8fee95677a3f76
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2023-03-20 16:57:23 +0530

    update

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-const DB = process.env.MONGO_UI;
+const DB =
+  'mongodb+srv://kulkarnisarang01:7776021397@cluster0.dj4jshn.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.set('strictQuery', false);
 mongoose
