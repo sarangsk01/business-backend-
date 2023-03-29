aa2c8144eb524c345ad0ab4b2a19980b420fccf5
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2023-03-29 16:06:40 +0530

    update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var mongoose = require('mongoose');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -61,6 +62,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
